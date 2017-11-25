5c0a4e453f64d45bd3f05ffc489f0aa0a0529c69
Earvin Li <earvin.tli@gmail.com>
2017-11-25 11:51:44 -0800

    cards in the openCards list can not be more than 2

--- js/app.js
+++ js/app.js
@@ -23,7 +23,6 @@ for (let i = 0; i < allCards.length; i++) {
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
